e594707077535b7168e902f0769392d86cf3dfaf
Luis Francisco Angulo Andrade <luisf117@gmail.com>
2019-07-09 00:29:38 -0700

    step 1: unicafe stats

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,16 +1,38 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
+// Button component
+const Button = ({clickHandler, btnText}) => <button onClick={clickHandler}>{btnText}</button>
+
+const Statistics = ({good, neutral, bad}) => {
+    return (
+        <div className="statistics-display">
+            <span><p>Good: {good}</p></span>
+            <span><p>Neutral: {neutral}</p></span>
+            <span><p>Bad: {bad}</p></span>
+        </div>
+    )
+}
+
 const App = () => {
     // save clicks of each button to own state
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
-  
+    
+    // Voting function
+    const vote = (handler, value) => handler(value+1)
+
     return (
-      <div>
-        code here
-      </div>
+        <>
+        <div>
+            <h1>Give feedback</h1>
+            <Button clickHandler={() => vote(setGood, good)} btnText="Good"/>
+            <Button clickHandler={() => vote(setNeutral, neutral)} btnText="Neutral"/>
+            <Button clickHandler={() => vote(setBad, bad)} btnText="Bad"/>
+        </div>
+        <Statistics good={good} neutral={neutral} bad={bad}/>
+        </>
     )
   }
 
