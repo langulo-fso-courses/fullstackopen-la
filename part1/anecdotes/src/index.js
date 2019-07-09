93ba5f060722d2ba8ec8e50f430de40fce7aab51
Luis Francisco Angulo Andrade <luisf117@gmail.com>
2019-07-09 14:46:23 -0700

    Step 3 - voting and save state

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -9,33 +9,59 @@ const App = props => {
   // the selected anecdote (index integer)
   const [selected, setSelected] = useState(0);
   const [votes, setVotes] = useState({
-      0:0,
-      1:0,
-      2:0,
-      3:0,
-      4:0,
-      5:0,
-      6:0
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0
   });
+  const [favorite, setFavorite] = useState({ quote: 0, score: 0 });
 
-  // const a = anecdotes[1]
-  const nextAnecdote = () => setSelected(Math.floor(Math.random() * anecdotes.length));
+  const nextAnecdote = () =>
+    setSelected(Math.floor(Math.random() * anecdotes.length));
 
   // Rock the vote!
   const vote = () => {
-    const newVotes = { ...votes }
-    newVotes[selected] +=1
-    setVotes(newVotes)
+    const newVotes = { ...votes };
+    newVotes[selected] += 1;
+    setVotes(newVotes);
+
+    // To set the favorite, we must compare the current favorite to all the other quotes
+    // If we find a quote that has more votes than the current favorite
+    // we must replace the favorite with the quote with higher votes
+    const entries = Object.entries(newVotes);
+    for (const [quote, vote] of entries) {
+      if (vote > favorite["score"]) {
+        const newFavorite = { quote: Number(quote), score: vote }
+        console.log('previous favorite', favorite)
+        console.log('new favorite', newFavorite)
+        setFavorite(newFavorite);
+      }
+    }
   };
 
   return (
     <div>
-      <div>{anecdotes[selected]}</div>
+      <div className="anecdote-selected">
+        <h1>Anecdote of the day</h1>
+        <div>{anecdotes[selected]}</div>
+        <span>
+          <p>This anecdote has {votes[selected]} votes</p>
+        </span>
+      </div>
       <span>
-          <Button handleClick={nextAnecdote} btnTxt="Next anecdote" />
-          <Button handleClick={vote} btnTxt="Vote for this anecdote" />
+        <Button handleClick={nextAnecdote} btnTxt="Next anecdote" />
+        <Button handleClick={vote} btnTxt="Vote for this anecdote" />
       </span>
-      <span><p>This anecdote has {votes[selected]} votes</p></span>
+      <div className="anecdote-favorite">
+        <h1>Anecdote with most votes</h1>
+        {console.log('current favorite', favorite)}
+        {console.log('quotes', anecdotes)}
+        <div>{anecdotes[favorite.quote]}</div>
+        <p>has {favorite.score} votes</p>
+      </div>
     </div>
   );
 };
