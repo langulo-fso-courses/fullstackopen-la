5d33cf37b9fbfe32d562affe152641486d27a23d
Luis Francisco Angulo Andrade <luisf117@gmail.com>
2019-07-09 01:58:48 -0700

    step 6 - HTML table for stats

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -6,33 +6,50 @@ const Button = ({ clickHandler, btnText }) => (
   <button onClick={clickHandler}>{btnText}</button>
 );
 
-const Statistics = ({good, neutral, bad}) => {
-    const votes = good + neutral + bad
-    // PANIC! We have no votes! We can't continue! Just return a message! AAAAAAHHH!!!
-    if (votes === 0) return <div><p>No feedback given</p></div>
+const Statistics = ({ good, neutral, bad }) => {
+  const votes = good + neutral + bad;
+  // PANIC! We have no votes! We can't continue! Just return a message! AAAAAAHHH!!!
+  if (votes === 0)
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    );
 
-    const score = good - bad
-    const average = score/votes
-    const positives = good/votes
+  const score = good - bad;
+  const average = score / votes;
+  const positives = good / votes;
 
-    return(
-        <div className="statistics-display">
-            <Statistic name="Good" value={good} />
-            <Statistic name="Neutral" value={neutral} />
-            <Statistic name="Bad" value={bad} />
-            <Statistic name="All" value={votes} />
-            <Statistic name="Average" value={average}/>
-            <Statistic name="Positive" value={positives.toString() + "%"}/>
-        </div>
-    )
-}
+  return (
+    <>
+      <h1>
+        <b>Statistics</b>
+      </h1>
+      <table>
+        <tbody>
+          <Statistic name="Good" value={good} />
+          <Statistic name="Neutral" value={neutral} />
+          <Statistic name="Bad" value={bad} />
+          <Statistic name="All" value={votes} />
+          <Statistic name="Average" value={average} />
+          <Statistic name="Positive" value={positives.toString() + "%"} />
+        </tbody>
+      </table>
+    </>
+  );
+};
 
 // Remember the args will be destructured here (I.E. "props.name, props.value")
-const Statistic = ({name, value}) => {
-    return(
-        <div><span className="statistic-span"><b>{name}: </b>{value}</span></div>
-    )
-}
+const Statistic = ({ name, value }) => {
+  return (
+    <tr>
+      <td>
+        <b>{name}:</b>
+      </td>
+      <td>{value}</td>
+    </tr>
+  );
+};
 
 const App = () => {
   // save clicks of each button to own state
