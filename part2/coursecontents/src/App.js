b879caf3c3b9d9ec992285b934ba021fd5e2bbc3
Luis Francisco Angulo Andrade <luisf117@gmail.com>
2019-07-09 22:53:58 -0700

    Steps 7, 8, 9 and separate Course module

--- part2/coursecontents/src/App.js
+++ part2/coursecontents/src/App.js
@@ -2,8 +2,9 @@ import React from "react";
 import Course from "./components/Course";
 
 const App = () => {
-  const course = {
+  const courses = [{
     name: "Half Stack application development",
+    id:1,
     parts: [
       {
         name: "Fundamentals of React",
@@ -21,11 +22,28 @@ const App = () => {
         id: 3
       }
     ]
-  };
+  },
+  {
+    name: 'Node.js',
+    id: 2,
+    parts: [
+      {
+        name: 'Routing',
+        exercises: 3,
+        id: 1
+      },
+      {
+        name: 'Middlewares',
+        exercises: 7,
+        id: 2
+      }
+    ]
+  }
+]
 
   return (
     <div>
-      <Course course={course} />
+      {courses.map((course) => <Course course={course} key={course.id} /> )}
     </div>
   );
 };
