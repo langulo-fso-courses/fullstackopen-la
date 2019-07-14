f01de714633b6b721654ce3b6264255da9c6160d
Luis Francisco Angulo Andrade <luisf117@gmail.com>
2019-07-13 20:32:47 -0700

    Step2.1 - selecting a country

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
 
+// The country search component
 const CountrySearch = ({ searchedCountry, setSearchedC }) => {
   const handler = event => setSearchedC(event.target.value);
   return (
@@ -20,38 +21,60 @@ const CountrySearch = ({ searchedCountry, setSearchedC }) => {
 
 // The display for the results of the search
 const CountryDisplay = ({ countries, searchedCountry }) => {
+  // Selected country is only relevant for the display component, hence that bit of state is here
+  const [selectedCountry, setSelectedC] = useState(null);
+
   const filter = country =>
     country.name.toLowerCase().includes(searchedCountry.toLowerCase());
   const matches = countries.filter(filter);
 
   let elements = null;
+
+  // Checks for how many results you have. Ugly chain of ifs, ewwww
   if (matches.length > 10) {
     elements = <p>Too many matches, specify another filter</p>;
   } else if (matches.length < 1) {
     elements = <p>No countries found, try changing your search terms</p>;
   } else if (matches.length === 1) {
-    elements = <CountryDetail country={matches[0]} />
+    elements = <CountryDetail country={matches[0]} />;
   } else {
     elements = matches.map(country => (
-      <CountryShort country={country} key={country.numericCode} />
+      <CountryShort
+        country={country}
+        key={country.numericCode}
+        setSelectedC={setSelectedC}
+      />
     ));
   }
   return <div className="countries-display">{elements}</div>;
 };
 
-const CountryShort = ({ country }) => {
-  return <p>{country.name}</p>;
+const CountryShort = ({ country, setSelectedC }) => {
+  return (
+    <div className="country-short">
+      <span><p>{country.name}</p> <button onClick={setSelectedC} >Show</button></span>
+    </div>
+  );
 };
 
 const CountryDetail = ({ country }) => {
   return (
     <div className="country-detail">
       <h1>{country.name}</h1>
-      <span><b>Capital: </b>{country.capital}</span><br/>
-      <span><b>Population: </b>{country.population}</span>
+      <span>
+        <b>Capital: </b>
+        {country.capital}
+      </span>
+      <br />
+      <span>
+        <b>Population: </b>
+        {country.population}
+      </span>
       <h3>Languages</h3>
       <ul>
-        {country.languages.map((language => <li>{language.name}</li>))}
+        {country.languages.map(language => (
+          <li>{language.name}</li>
+        ))}
       </ul>
       <img src={country.flag} alt="country flag" width="480" length="640" />
     </div>
@@ -64,7 +87,6 @@ function App() {
   // State
   const [searchedCountry, setSearchedC] = useState("");
   const [countries, setCountries] = useState([]);
-  // const [selectedCountry, setSelectedC] = useState(0);
 
   // Request the whole list of countries from the endpoint and save it as state
 
