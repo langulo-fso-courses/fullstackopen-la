06578b929da3d91b77a4ffac0889743ac5d185a5
Luis Francisco Angulo Andrade <luisf117@gmail.com>
2019-07-13 19:38:57 -0700

    step1.1 - state, request and searchfilter works

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,36 +1,67 @@
-import React, { useState, useEffect } from 'react';
-import Axios from 'axios';
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
-const CountrySearch = ({searchedCountry, setSearchedC}) => {
-  console.log("country search")
-  return(
+const CountrySearch = ({ searchedCountry, setSearchedC }) => {
+  const handler = event => setSearchedC(event.target.value);
+  return (
     <div className="country-search">
-      <input type="text" value={searchedCountry} onChange={setSearchedC} />
+      <span>
+        <label htmlFor="searched">Find countries: </label>
+        <input
+          name="searched"
+          type="text"
+          value={searchedCountry}
+          onChange={handler}
+        />
+      </span>
     </div>
-  )
-}
+  );
+};
+
+// The display for the results of the search
+const CountryDisplay = ({ countries, searchedCountry }) => {
+  const filter = country =>
+    country.name.toLowerCase().includes(searchedCountry);
+  const matches = countries.filter(filter);
+
+  let elements = null;
+  if (matches.length > 10) {
+    elements = <p>Too many matches, specify another filter</p>;
+  } else if (matches.length < 1) {
+    elements = <p>No countries found, try changing your search terms</p>;
+  } else {
+    elements = matches.map(country => <CountryShort country={country} key={country.alpha3Code} />);
+  }
+  return <div className="countries-display">{elements}</div>;
+};
+
+const CountryShort = ({ country }) => {
+  return <p>{country.name}</p>;
+};
 
 function App() {
+  // TODO: DON'T use the namesearch in the API, it might trigger too many requests too fast and you'll get blacklisted for awhile
+
   // State
-  const [searchedCountry, setSearchedC] = useState('');
+  const [searchedCountry, setSearchedC] = useState("");
   const [countries, setCountries] = useState([]);
-  const [selectedCountry, setSelectedC] = useState(0);
+  // const [selectedCountry, setSelectedC] = useState(0);
 
-  // Request effect
-  const apiHookAll = ()=>{
-    Axios.get('https://restcountries.eu/rest/v2/all')
-    .then((res) => {
-      console.log(res.data)
+  // Request the whole list of countries from the endpoint and save it as state
+
+  useEffect(() => {
+    Axios.get("https://restcountries.eu/rest/v2/all").then(res =>
       setCountries(res.data)
-    })
-  }
-
-  useEffect(apiHookAll, [])
+    );
+  }, []);
 
   return (
     <div className="App">
-      <CountrySearch  />
-      <div></div>
+      <CountrySearch
+        searchedCountry={searchedCountry}
+        setSearchedC={setSearchedC}
+      />
+      <CountryDisplay countries={countries} searchedCountry={searchedCountry} />
     </div>
   );
 }
