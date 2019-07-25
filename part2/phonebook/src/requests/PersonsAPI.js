de37b1032a67657176a6c2a9e5fbb7e502f76528
Luis Francisco Angulo Andrade <luisf117@gmail.com>
2019-07-24 20:13:43 -0700

    Config for deployment to heroku

--- part2/phonebook/src/requests/PersonsAPI.js
+++ part2/phonebook/src/requests/PersonsAPI.js
@@ -1,32 +1,32 @@
 import Axios from 'axios';
-const baseURL = `http://localhost:3001/`
+// Development URL
+// const baseURL = `http://localhost:3001/`;
+// Deployment URL
+const baseURL = `/api/persons`;
 
-// An API
 const getAll = () => {
-    return Axios.get(`${baseURL}persons`)
+    return Axios.get(`${baseURL}`)
     .then(res => res.data)
 }
 
 const getOne = id => {
-    return Axios.get(`${baseURL}persons/${id}`)
+    return Axios.get(`${baseURL}/${id}`)
     .then(res => res.data)
 }
 
 const update = (id, newPerson) => {
-    return Axios.put(`${baseURL}persons/${id}`, newPerson)
+    return Axios.put(`${baseURL}/${id}`, newPerson)
     .then(res => res.data)
 }
 
 const create = newPerson => {
-    return Axios.post(`${baseURL}persons/`, newPerson)
+    return Axios.post(`${baseURL}/`, newPerson)
     .then(res => res.data)
 }
 
-// Axios destroy function returns no data, so that return is a bad idea
-// This points to a more general problem in designing a wrapper around AJAX libraries
-// Which I'm not knowledgeable enough to solve at this time - L.A.
+// TODO: Axios destroy function returns no data, so that return is a bad idea
 const destroy = id => {
-    return Axios.delete(`${baseURL}persons/${id}`)
+    return Axios.delete(`${baseURL}/${id}`)
     .then(res => res.data)
 }
 
